1bb270090711fb763b87243fed925015570630f8
Gustavo <gustavo-dev@hotmail.com>
2021-12-23 20:09:13 -0300

    Adicionado [Rotas -> Controller -> View] e README.md modificado

---
+++ app/Http/Controllers/AdressController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class AdressController extends Controller
+{
+    public function index() {
+        return view('search');
+    }
+
+    public function searchAction(Request $request) {
+        $cep = $request->input('cep');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\AdressController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::redirect('/', '/home');
+
+Route::get('/home', [AdressController::class, 'index'])->name('home');
+
+Route::get('/search', [AdressController::class, 'searchAction'])->name('search');
+
